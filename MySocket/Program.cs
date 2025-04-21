b74c819c40649781262ff94395c9ba2f73bbbff3
IVAN IGNACIO BRITO MEDINA <ibrito@estudiantec.cr>
2025-04-21 15:07:38 -0600

    Iniciar las pruebas de SocketClient

--- MySocket/Program.cs
+++ MySocket/Program.cs
@@ -2,6 +2,7 @@
 using Networking;
 using Mensajes;
 using Interfaces;
+using Networking;
 namespace MySocket
 {
     internal class Program
@@ -9,13 +10,8 @@ namespace MySocket
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            // Enviar un mensaje
-            Mensaje mensaje = new Mensaje("Mi primer mensaje", "Este es el cuerpo del mensaje");
-            Mensaje.Escuchar(mensaje);
-            Console.WriteLine(mensaje.ToString());
-            Mensaje mensaje2 = new Mensaje("Mi segundo mensaje", "Este es el cuerpo del 2do mensaje");
-            Console.Write(mensaje2.ToString());
-            Console.WriteLine(Mensaje.listaDeMensajes);
+            SocketClient cliente = new SocketClient();
+            cliente.Conectar("127.0.0.1", 5000);
         }
     }
 
