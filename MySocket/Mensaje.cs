48b785b06d79fab6d677eb8a859864040a85f43f
IVAN IGNACIO BRITO MEDINA <ibrito@estudiantec.cr>
2025-04-21 14:30:53 -0600

    Probar el resto de los metodos de Mensaje, eliminar el proyecto inecesario Mensaje.

--- MySocket/Mensaje.cs
+++ MySocket/Mensaje.cs
@@ -1,27 +1,28 @@
-﻿using Listas;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Listas;
 
-namespace MySocket
+namespace Mensajes
 {
     public class Mensaje
     {
-        private static int contadorId;
-        private static Lista<Mensaje> listaDeMensajes = new ListaDobleEnlazada<Mensaje>();
-        public int idMensaje { get; set; }
-        public string encabezado { get; set; }
+        internal static int id { get; private set; }
+        public string? usuario { get; internal set; }
+        public string? encabezado { get; internal set; }
+        public DateTime fecha { get; internal set; }
+        internal static Lista<Mensaje> listaDeMensajes = new ListaDobleEnlazada<Mensaje>();
+
+        public int idMensaje { get; private set; }
         public string cuerpo { get; set; }
 
         public Mensaje(string encabezado, string cuerpo)
         {
-            //if (contadorId == 0) contadorId = 0;
-            idMensaje = contadorId;
+            idMensaje = id++;
             this.encabezado = encabezado;
             this.cuerpo = cuerpo;
-            contadorId++;
         }
 
         public static void Escuchar(Mensaje mensaje)
@@ -29,5 +30,28 @@ namespace MySocket
             listaDeMensajes.Anadir(mensaje);
         }
 
+        public override string ToString()
+        {
+            return $"[{idMensaje}] {encabezado}: {cuerpo}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is not Mensaje otro) return false;
+            return this.idMensaje == otro.idMensaje &&
+                   this.usuario == otro.usuario &&
+                   this.encabezado == otro.encabezado &&
+                   this.cuerpo == otro.cuerpo &&
+                   this.fecha == otro.fecha;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(idMensaje, usuario, encabezado, fecha);
+        }
+        public static void Limpiar()
+        {
+            listaDeMensajes.Vaciar();
+            id = 0;
+        }
     }
 }
