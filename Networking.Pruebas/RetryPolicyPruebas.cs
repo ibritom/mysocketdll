554aa287f8b97dab4fe8bdb1b96c9e6f8c22997f
IVAN IGNACIO BRITO MEDINA <ibrito@estudiantec.cr>
2025-04-21 13:58:26 -0600

    Añadir proyecto de pruebas para networking

---
+++ Networking.Pruebas/RetryPolicyPruebas.cs
@@ -0,0 +1,11 @@
+namespace Networking.Pruebas
+{
+    [TestClass]
+    public class RetryPolicyPruebas
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
